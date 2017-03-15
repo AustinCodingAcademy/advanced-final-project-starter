668ef29c1d39bdb9c351911afab794cef2ccfd16
trevorlujan123 <trevorlujan@live.com>
2017-03-15 16:12:27 -0500

    intial commit

--- src/routes/AuthenticationRoutes.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const jwt = require('jwt-simple');
-const User = require('../models/UserModel');
-const bcrypt = require('bcrypt');
--- src/services/passport.js
+++
@@ -1,5 +0,0 @@
-const bcrypt = require('bcrypt');
-const passport = require('passport');
-const User = require('../models/UserModel');
-const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
-const LocalStrategy = require('passport-local');
