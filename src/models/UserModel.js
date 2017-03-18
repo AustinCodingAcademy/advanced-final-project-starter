4eccf25ca4461d7484b5f6e3d248fd4d90964bd6
nicolemichelle88 <daugereaux@gmail.com>
2017-03-18 16:53:27 -0500

    first commit

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const bcrypt = require('bcrypt');
-
+// making changes to open a PR
 const userSchema = new Schema({
   username: {
     type: String,
